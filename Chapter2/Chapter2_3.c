783ab4f6b2212e5beae841d7c0a2caee5d86a51c
Mint-Children <102007444+Mint-Children@users.noreply.github.com>
2023-04-16 01:01:23 +0900

    Add files via upload

---
+++ Chapter2/Chapter2_3.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	printf("-------------------------------------------\n");
+	printf("                  학과: 컴퓨터공학과       \n");
+	printf("                  학번: 0001               \n");
+	printf("                  성명: 홍길동             \n");
+	printf("-------------------------------------------\n");
+
+	return 0;
+}
---
+++ Chapter2/Chapter2_5.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	printf("7+8=%d\n", 7 + 8);
+	printf("7-8=%d\n", 7 - 8);
+	printf("7*8=%d\n", 7 * 8);
+	printf("7/8=%d\n", 7 / 8);
+
+	return 0;
+}
